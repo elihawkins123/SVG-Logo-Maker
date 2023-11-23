c318e3543115ba3ef5c5501e5f69d3c0ca674347
elihawkins123 <elizabethrhawkins123@gmail.com>
2023-11-22 22:05:32 -0700

    code SVG with TA help, a huge thank you for Philip

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const {Circle, Square, Triangle} = require("./lib/shapes.js")
+inquirer.prompt ([{
+    type: 'input',
+    name: 'text', 
+    message: 'What is your three letter text?'
+}, 
+{
+    type:'input', 
+    name:'textColor',
+    message: 'What is the text color?', 
+     
+}, 
+{
+    type:'list', 
+    message: 'What is the shape?',
+    name: 'shape', 
+    choices: ['circle', 'square', 'triangle'],
+}, 
+{
+    type: 'input', 
+    name: 'shapeColor', 
+    message: 'What is your shape color?'
+    }
+])
+.then((data) => {
+    let output= ""
+    const filename = `./example/logo.svg`;
+    if(data.shape === "circle"){
+        const text = new Circle (data.text, data.textColor, data.shapeColor)
+        output= text.render()
+    }
+    if(data.shape === 'square'){
+        const text = new Square (data.text, data.textColor, data.shapeColor)
+        output= text.render()
+    }
+    if(data.shape === 'triangle'){
+        const text = new Triangle (data.text, data.textColor, data.shapeColor)
+        output= text.render()
+    }
+    fs.writeFile(filename, output, (err) =>
+    err ? console.log(err) : console.log('Success!')
+    ); 
+}); 
